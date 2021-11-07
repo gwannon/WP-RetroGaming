701e134a8b218b01fe6ecf72f2454468d4e5cc20
Gwannon <monclus.jorge@gmail.com>
2021-11-07 19:26:34 +0100

    Administracion de plantilla

--- wp-retrogaming/lib/custom_posts.php
+++ wp-retrogaming/lib/custom_posts.php
@@ -10,87 +10,87 @@ function wp_retrogaming_show_custom_fields() { //Show box
   $type = get_post_type($post->ID);
 	$fields = get_wp_retrogaming_custom_fields ($type); ?>
 		<div>
-		<?php foreach ($fields as $field => $datos) { ?>
-			<?php if($datos['tipo'] != 'repeater') { ?><div style="width: calc(50% - 10px); float: left; padding: 5px;"><?php } else { ?><div style="width: calc(100% - 10px); float: left; padding: 5px;"><?php } ?>
-				<p><b><?php echo $datos['titulo']; ?></b></p>
-				<?php if($datos['tipo'] == 'text' || $datos['tipo'] == 'link') { ?>
-					<input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
-				<?php } else if($datos['tipo'] == 'date') { ?>
-					<input type="date" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 50%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
-				<?php } else if($datos['tipo'] == 'hidden') { ?>
-					<input disabled="disabled" type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 50%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
-				<?php } else if($datos['tipo'] == 'image') { ?>
-					<input type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="input_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 80%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value='<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>' />
-					<a href="#" id="button_devide_<?php echo $field; ?>" class="button insert-media add_media" data-editor="input_<?php echo $type; ?>_<?php echo $field; ?>" title="<?php _e("Añadir fichero", 'the7mk2'); ?>"><span class="wp-media-buttons-icon"></span> <?php _e("Añadir fichero", 'the7mk2'); ?></a>
-					<script>
-						jQuery(document).ready(function () {			
-							jQuery("#input_<?php echo $type; ?>_<?php echo $field; ?>").change(function() {
-								a_imgurlar = jQuery(this).val().match(/<a href=\"([^\"]+)\"/);
-								img_imgurlar = jQuery(this).val().match(/<img[^>]+src=\"([^\"]+)\"/);
-								if(img_imgurlar !==null ) jQuery(this).val(img_imgurlar[1]);
-								else  jQuery(this).val(a_imgurlar[1]);
-							});
-						});
-					</script>
-				<?php } else if($datos['tipo'] == 'text') { ?>
-					<input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
-				<?php } else if($datos['tipo'] == 'textarea') { ?>
-					<?php $settings = array( 'media_buttons' => true, 'quicktags' => true, 'textarea_rows' => 5 ); ?>
-					<?php wp_editor( get_post_meta( $post->ID, '_'.$type.'_'.$field, true ), '_'.$type.'_'.$field, $settings ); ?>
-				<?php } else if ($datos['tipo'] == 'select') { ?>
-					<select name="_<?php echo $type; ?>_<?php echo $field; ?>">
-						<?php foreach($datos['valores'] as $key => $value) { ?>
-							<option value="<?php echo $key; ?>"<?php if ($key == get_post_meta( $post->ID, '_'.$type.'_'.$field, true )) echo " selected='selected'"; ?>><?php echo $value; ?></option>
-						<?php } ?>	
-					</select>
-				<?php } else if ($datos['tipo'] == 'multiple') { ?>
-					<select name="_<?php echo $type; ?>_<?php echo $field; ?>[]" multiple="multiple">
-						<?php foreach($datos['valores'] as $key => $value) { ?>
-							<option value="<?php echo $key; ?>"<?php if (in_array($key, get_post_meta( $post->ID, '_'.$type.'_'.$field, true ))) echo " selected='selected'"; ?>><?php echo $value; ?></option>
-						<?php } ?>	
-					</select>
-				<?php }  else if ($datos['tipo'] == 'checkbox') { ?>
-							<?php $results = get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>
-							<?php foreach($datos['valores'] as $key => $value) { ?>
-								<input type="checkbox" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" name="_<?php echo $type; ?>_<?php echo $field; ?>[]" value="<?php echo $key; ?>" <?php if(is_array($results) && in_array($key, $results)) { echo "checked='checked'"; } ?> /> <?php echo $value; ?><br/>
-						<?php } ?>	
-				<?php } else if ($datos['tipo'] == 'repeater') { $rest = get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); if(isset($datos['max'])) $max = $datos['max']; else $max = (count($rest) < 2 ? 2 : (count($rest) + 1)); ?>
-      <div id="repeater_<?php echo $field; ?>">
-        <?php for ($i = 0; $i < $max; $i ++) { ?>
-            <?php foreach($datos['fields'] as $key => $subfields) { ?>
-              <?php if($subfields['tipo'] == 'image') { ?>
-                <input type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>" id="input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" style="width: 80%;" name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]" value="<?php echo $rest[$i][$key]; ?>" placeholder="<?php echo $subfields['titulo']; ?>" />
-                <a href="#" id="button_devide_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" class="button insert-media add_media" data-editor="input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" title="<?php _e("Añadir fichero"); ?>"><span class="wp-media-buttons-icon"></span> <?php _e("Añadir fichero"); ?></a>
-                <script>
-                  jQuery(document).ready(function () {			
-                    jQuery("#input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>").change(function() {
-                      a_imgurlar = jQuery(this).val().match(/<a href=\"([^\"]+)\"/);
-                      img_imgurlar = jQuery(this).val().match(/<img[^>]+src=\"([^\"]+)\"/);
-                      if(img_imgurlar !==null ) jQuery(this).val(img_imgurlar[1]);
-                      else  jQuery(this).val(a_imgurlar[1]);
+      <?php foreach ($fields as $field => $datos) { ?>
+        <?php if($datos['tipo'] != 'repeater') { ?><div style="width: calc(50% - 10px); float: left; padding: 5px;"><?php } else { ?><div style="width: calc(100% - 10px); float: left; padding: 5px;"><?php } ?>
+          <p><b><?php echo $datos['titulo']; ?></b></p>
+          <?php if($datos['tipo'] == 'text' || $datos['tipo'] == 'link') { ?>
+            <input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
+          <?php } else if($datos['tipo'] == 'date') { ?>
+            <input type="date" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 50%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
+          <?php } else if($datos['tipo'] == 'hidden') { ?>
+            <input disabled="disabled" type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 50%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
+          <?php } else if($datos['tipo'] == 'image') { ?>
+            <input type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="input_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 80%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value='<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>' />
+            <a href="#" id="button_devide_<?php echo $field; ?>" class="button insert-media add_media" data-editor="input_<?php echo $type; ?>_<?php echo $field; ?>" title="<?php _e("Añadir fichero", 'the7mk2'); ?>"><span class="wp-media-buttons-icon"></span> <?php _e("Añadir fichero", 'the7mk2'); ?></a>
+            <script>
+              jQuery(document).ready(function () {			
+                jQuery("#input_<?php echo $type; ?>_<?php echo $field; ?>").change(function() {
+                  a_imgurlar = jQuery(this).val().match(/<a href=\"([^\"]+)\"/);
+                  img_imgurlar = jQuery(this).val().match(/<img[^>]+src=\"([^\"]+)\"/);
+                  if(img_imgurlar !==null ) jQuery(this).val(img_imgurlar[1]);
+                  else  jQuery(this).val(a_imgurlar[1]);
+                });
+              });
+            </script>
+          <?php } else if($datos['tipo'] == 'text') { ?>
+            <input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>" value="<?php echo get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>" />
+          <?php } else if($datos['tipo'] == 'textarea') { ?>
+            <?php $settings = array( 'media_buttons' => true, 'quicktags' => true, 'textarea_rows' => 5 ); ?>
+            <?php wp_editor( get_post_meta( $post->ID, '_'.$type.'_'.$field, true ), '_'.$type.'_'.$field, $settings ); ?>
+          <?php } else if ($datos['tipo'] == 'select') { ?>
+            <select name="_<?php echo $type; ?>_<?php echo $field; ?>">
+              <?php foreach($datos['valores'] as $key => $value) { ?>
+                <option value="<?php echo $key; ?>"<?php if ($key == get_post_meta( $post->ID, '_'.$type.'_'.$field, true )) echo " selected='selected'"; ?>><?php echo $value; ?></option>
+              <?php } ?>	
+            </select>
+          <?php } else if ($datos['tipo'] == 'multiple') { ?>
+            <select name="_<?php echo $type; ?>_<?php echo $field; ?>[]" multiple="multiple">
+              <?php foreach($datos['valores'] as $key => $value) { ?>
+                <option value="<?php echo $key; ?>"<?php if (in_array($key, get_post_meta( $post->ID, '_'.$type.'_'.$field, true ))) echo " selected='selected'"; ?>><?php echo $value; ?></option>
+              <?php } ?>	
+            </select>
+          <?php }  else if ($datos['tipo'] == 'checkbox') { ?>
+            <?php $results = get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); ?>
+            <?php foreach($datos['valores'] as $key => $value) { ?>
+              <input type="checkbox" class="_<?php echo $type; ?>_<?php echo $field; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>" name="_<?php echo $type; ?>_<?php echo $field; ?>[]" value="<?php echo $key; ?>" <?php if(is_array($results) && in_array($key, $results)) { echo "checked='checked'"; } ?> /> <?php echo $value; ?><br/>
+            <?php } ?>	
+        <?php } else if ($datos['tipo'] == 'repeater') { $rest = get_post_meta( $post->ID, '_'.$type.'_'.$field, true ); if(isset($datos['max'])) $max = $datos['max']; else $max = (count($rest) < 2 ? 2 : (count($rest) + 1)); ?>
+        <div id="repeater_<?php echo $field; ?>">
+          <?php for ($i = 0; $i < $max; $i ++) { ?>
+              <?php foreach($datos['fields'] as $key => $subfields) { ?>
+                <?php if($subfields['tipo'] == 'image') { ?>
+                  <input type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>" id="input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" style="width: 80%;" name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]" value="<?php echo $rest[$i][$key]; ?>" placeholder="<?php echo $subfields['titulo']; ?>" />
+                  <a href="#" id="button_devide_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" class="button insert-media add_media" data-editor="input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" title="<?php _e("Añadir fichero"); ?>"><span class="wp-media-buttons-icon"></span> <?php _e("Añadir fichero"); ?></a>
+                  <script>
+                    jQuery(document).ready(function () {			
+                      jQuery("#input_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>").change(function() {
+                        a_imgurlar = jQuery(this).val().match(/<a href=\"([^\"]+)\"/);
+                        img_imgurlar = jQuery(this).val().match(/<img[^>]+src=\"([^\"]+)\"/);
+                        if(img_imgurlar !==null ) jQuery(this).val(img_imgurlar[1]);
+                        else  jQuery(this).val(a_imgurlar[1]);
+                      });
                     });
-                  });
-                </script>
-              <?php } else if($subfields['tipo'] == 'textarea') { ?>
+                  </script>
+                <?php } else if($subfields['tipo'] == 'textarea') { ?>
                   <?php $settings = array( 'media_buttons' => false, 'quicktags' => true, 'textarea_rows' => 5 );  ?>
                   <?php wp_editor($rest[$i][$key], '_'.$type.'_'.$field.'['.$i.']['.$key.']', $settings ); ?>
-              <?php } else if ($subfields['tipo'] == 'select') { ?>
-                <select name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]">
-                  <?php foreach($subfields['valores'] as $key => $value) { ?>
-                    <option value="<?php echo $key; ?>"<?php if ($key == $rest[$i][$key]) echo " selected='selected'"; ?>><?php echo $value; ?></option>
-                  <?php } ?>	
-                </select>
-              <?php } else { ?>
-                <input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]" value="<?php echo $rest[$i][$key]; ?>" placeholder="<?php echo $subfields['titulo']; ?>" />
+                <?php } else if ($subfields['tipo'] == 'select') { ?>
+                  <select name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]">
+                    <?php foreach($subfields['valores'] as $key => $value) { ?>
+                      <option value="<?php echo $key; ?>"<?php if ($key == $rest[$i][$key]) echo " selected='selected'"; ?>><?php echo $value; ?></option>
+                    <?php } ?>	
+                  </select>
+                <?php } else { ?>
+                  <input  type="text" class="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>" id="_<?php echo $type; ?>_<?php echo $field; ?>_<?php echo $key; ?>_<?php echo $i; ?>" style="width: 100%;" name="_<?php echo $type; ?>_<?php echo $field; ?>[<?php echo $i; ?>][<?php echo $key; ?>]" value="<?php echo $rest[$i][$key]; ?>" placeholder="<?php echo $subfields['titulo']; ?>" />
+                <?php } ?>
               <?php } ?>
-            <?php } ?>
-          <hr style="margin: 30px 0px;"/>
-        <?php } ?>
+            <hr style="margin: 30px 0px;"/>
+          <?php } ?>
+        </div>
+      <?php } ?>
       </div>
     <?php } ?>
-		</div>
-	<?php } ?>
-	<div style="clear: both;"></div>
+    <div style="clear: both;"></div>
 	</div> <?php
 }
 
@@ -119,7 +119,7 @@ function wp_retrogaming_save_custom_fields( $post_id ) { //Save changes
 function array_remove_empty($haystack) {
   foreach ($haystack as $key => $value) {
     if (is_array($value)) {
-        $haystack[$key] = array_remove_empty($haystack[$key]);
+      $haystack[$key] = array_remove_empty($haystack[$key]);
     }
     if (empty($haystack[$key])) {
         unset($haystack[$key]);
